0364c92cd5992c2ed2e884d1f6c83e0ae6e2b9ff
Reinaldo Verdugo <rverdugo@solsteace.com>
2016-03-06 22:48:24 -0430

    Añadidas modas y script en R

---
+++ data/proy1.R
@@ -0,0 +1,22 @@
+datos = read.table("crx.data", header= TRUE, sep= ",")
+attach(datos)
+names(datos)
+ 
+moda<-function(var){
+	frec.var<-table(var)
+	valor<-which(frec.var==max(frec.var)) 
+	names(valor)
+}
+
+cat(" Moda de atributo 1 ", moda(att1))
+cat("\n Moda de atributo 2 ", moda(att2))
+cat("\n Moda de atributo 4 ", moda(att4))
+cat("\n Moda de atributo 5 ", moda(att5))
+cat("\n Moda de atributo 6 ", moda(att6))
+cat("\n Moda de atributo 7 ", moda(att7))
+cat("\n Moda de atributo 13 ", moda(att13))
+
+cat("\n Moda de atributo 2 ignorando ?:", "\n")
+summary(att2)
+
+#23.25
