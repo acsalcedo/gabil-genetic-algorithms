78c6e4db0279da3aaf6bc64f53bbfb1772c9e387
Reinaldo Verdugo <verdugoreinaldo@gmail.com>
2016-03-09 04:16:57 -0800

    Resultados de varias el metodo de seleccion de padres y sobrevivientes. Cambios en el archivo de promedio

--- src/avg.py
+++ src/avg.py
@@ -1,17 +1,23 @@
 import json
-
-extension = "cross0.6_mut0.01_par1_surv0_pop20_gen1000"
+cross = "0.9"
+mut = "0.01"
+extension = "cross"+cross+"_mut"+mut+"_par1_surv0_pop20_gen1000"
 testFile = open("../tests/"+extension, mode='r')
 data = json.load(testFile)
 avg = 0
 num = 0
-
+cont = 0
+print "cross: " + cross
+print "mut: " + mut
 for example in data:
-
+    if example['correct'] == 0:
+	continue
+    cont += 1
     avg += example['correct']
     with open("../data/test/"+example['time']+"_"+extension, mode='r') as f:
         data2 = json.load(f)
         num += len(data2)
-
+	
 
 print avg / float(num)
+print avg / float(cont)
