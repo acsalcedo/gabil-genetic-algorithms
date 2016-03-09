5daba189ac3d8f14b41d98b8a050f0cd2746b884
Andrea Salcedo <andreacsalcedo@gmail.com>
2016-03-09 05:58:28 -0430

    Agregados los resultados de población.

--- src/avg.py
+++ src/avg.py
@@ -1,12 +1,15 @@
 import json
 
-extension = "cross0.6_mut0.01_par1_surv0_pop20_gen1000"
+extension = "cross0.6_mut0.01_par1_surv1_pop60_gen1000"
 testFile = open("../tests/"+extension, mode='r')
 data = json.load(testFile)
 avg = 0
 num = 0
 
 for example in data:
+
+    if example['correct'] == 0:
+        continue
 
     avg += example['correct']
     with open("../data/test/"+example['time']+"_"+extension, mode='r') as f:
